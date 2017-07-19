50b2fb5c4fcb914883bd2579ba129827455b7088
billyjack1988 <billyjacktattoos@gmail.com>
2017-07-19 07:56:15 -0500

    passed test

--- random_names.rb
+++ random_names.rb
@@ -1,19 +1,21 @@
+
+
 def random_name(my_name, my_people)
     my_people << my_name    
-       p my_people.sample(2)
+       my_people.sample(2)
         if my_people.sample(2).length == 2
                true
         end
 end 
 
 
-# def names_at_random(my_name, my_people)
-#     random = []
-#         random_name.slice(2).to.a do 
-#            
-# random << my_people
-#         end
-#     p "#{random} here it is"
-# end
+def names_at_random(my_name, my_people)
+        my_people << my_name
+        random = my_people.shuffle.each_slice(2).to_a        
+        print " #{random} yo this is random array"
+         if my_people.shuffle.each_slice(2).to_a.length == 5
+             true
+        end
+end
 
 
--- random_names_testing.rb
+++ random_names_testing.rb
@@ -11,9 +11,16 @@ class Random_pairs_test < Minitest::Test
     # end
 
     def test_for_2_names 
-        my_name = ["billy"]
-        my_people = [["anthony"], ["gritts"], ["jim"], ["bobo"]]
+        my_name = "billy"
+        my_people = ["anthony", "gritts", "jim", "bobo"]
         assert_equal(true, random_name(my_name, my_people))
     end
+
+    def test_for_a_large_number_of_names
+        my_name = "billy"
+        my_people = ["anthony", "gritts", "jack", "jim", "bobo", "dale", "poblo", "ash", "misty"]
+        assert_equal(true, names_at_random(my_name, my_people))
+    end
+    
     
 end
