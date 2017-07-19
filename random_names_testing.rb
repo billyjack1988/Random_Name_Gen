e50a6045ba6f0e375003d18a60fc8899d58acad6
billyjack1988 <billyjacktattoos@gmail.com>
2017-07-19 10:34:23 -0500

    passed the odd number of names

--- random_names_testing.rb
+++ random_names_testing.rb
@@ -4,11 +4,11 @@ require_relative "random_names.rb"
 
 class Random_pairs_test < Minitest::Test
 
-    # def test_for_2_names 
-    #     my_name = "billy"
-    #     my_people = ["anthony", "gritts", "jim", "bobo"]
-    #     assert_equal(true, random_name(my_name, my_people))
-    # end
+    def test_for_2_names 
+        my_name = "billy"
+        my_people = ["anthony", "gritts", "jim", "bobo"]
+        assert_equal(true, random_name(my_name, my_people))
+    end
 
     def test_for_a_large_number_of_names
         my_name = "billy"
