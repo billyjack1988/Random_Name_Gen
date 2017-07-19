93c47862182b12d001fdb1eed43d2f982970c4ef
billyjack1988 <billyjacktattoos@gmail.com>
2017-07-19 10:10:14 -0500

    passed the odd number of names

--- random_names.rb
+++ random_names.rb
@@ -12,10 +12,17 @@ end
 def names_at_random(my_name, my_people)
         my_people << my_name
         random = my_people.shuffle.each_slice(2).to_a        
-        print " #{random} yo this is random array"
-         if my_people.shuffle.each_slice(2).to_a.length == 5
-             true
+        # p " #{random} yo this is random array"
+        #  p "#{my_people.length} length is here !!!!!"
+         if my_people.length % 2 == 0
+             random.length
+        else 
+        random[random.length - 2] << random[random.length - 1]
+        random[random.length - 2].flatten!
+        random.pop 
+        p random
+        random.length  
         end
+        
 end
-
 
--- random_names_testing.rb
+++ random_names_testing.rb
@@ -4,23 +4,23 @@ require_relative "random_names.rb"
 
 class Random_pairs_test < Minitest::Test
 
-    # def test_for_empty_array
+    # def test_for_2_names 
     #     my_name = "billy"
-	#     my_people = []
-	#     assert_equal([], random_name(my_name, my_people))
+    #     my_people = ["anthony", "gritts", "jim", "bobo"]
+    #     assert_equal(true, random_name(my_name, my_people))
     # end
-
-    def test_for_2_names 
-        my_name = "billy"
-        my_people = ["anthony", "gritts", "jim", "bobo"]
-        assert_equal(true, random_name(my_name, my_people))
-    end
 
     def test_for_a_large_number_of_names
         my_name = "billy"
         my_people = ["anthony", "gritts", "jack", "jim", "bobo", "dale", "poblo", "ash", "misty"]
-        assert_equal(true, names_at_random(my_name, my_people))
+        assert_equal(5, names_at_random(my_name, my_people))
     end
-    
+
+    def test_for_odd_number_of_names
+        my_name = "billy"
+        my_people = ["anthony", "gritts", "jack", "jim", "bobo", "dale", "poblo", "ash", "misty", "brock"]
+        assert_equal(5, names_at_random(my_name, my_people))
+    end 
+        
     
 end
